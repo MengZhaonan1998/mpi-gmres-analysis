b278f0bbe010a2acf07352c295845d28783656d4
Elias Stenhede Johansson <estenhedejohan@login03.delftblue.tudelft.nl>
2023-05-16 10:22:25 +0200

    add more benchmarks

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# Array of arguments
-args=(48 36 27 18 12 8 4 2 1)
+#Scaling benchmarks
+echo "With 32 processes, scaling benchmark:"
+sbatch -o slurm-scaling-32-$$.out -n 32 scaling_benchmarks.slurm
 
-# Loop through the arguments and submit SLURM job
+args=(80 64)
 for arg in "${args[@]}"; do
-		echo "Running with $arg processes:"
-    sbatch -n "$arg" run_benchmarks.slurm
+		echo "With $arg processes:"
+		sbatch -o slurm-communication-$arg-$$.out -n $arg research_account_communication_benchmarks.slurm
+done
+args=(48 36 27 18 12 8 4 2 1)
+for arg in "${args[@]}"; do
+		echo "With $arg processes:"
+    sbatch -o slurm-communication-$arg-$$.out -n "$arg" communication_benchmarks.slurm
+    sbatch -o slurm-precond-$arg-$$.out -n "$arg" precond_benchmarks.slurm
 done
 
